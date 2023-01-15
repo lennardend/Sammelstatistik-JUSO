7a0233f14e17aeaacb0912956c10b6855d09d6af
lennardend <lennard.end02@outlook.com>
2023-01-15 23:44:14 +0100

    fixed error with static files

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', `${__dirname}/public`)
+app.set('views', `${__dirname}/public`);
+
+app.use('/public', express.static(`${__dirname}/public`));
+
 
 app.get('/', (req, res) => {
     res.render('stats', {
-        test: 'Deine Mutter'
+        test: 'asdfadsf'
     })
 });
 
