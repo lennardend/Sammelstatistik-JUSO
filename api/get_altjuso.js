7db37076f472f999925202b525d8fcf439d3803f
Lennard End <endle@bbcag.ch>
2023-01-20 16:24:46 +0100

    Connectors für Alt-JUSO und Top 3 Listen erstellt

---
+++ api/get_altjuso.js
@@ -0,0 +1,5 @@
+const db = require('./connect_db.js')
+
+// TODO: JSON mit top 3 alt-jusos
+
+module.exports = { items }
---
+++ api/get_top3.js
@@ -0,0 +1,5 @@
+const db = require('./connect_db.js')
+
+//JSON mit Top 3 Jusos
+
+module.exports = { items }
