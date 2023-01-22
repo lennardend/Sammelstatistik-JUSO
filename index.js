3f13f7e13d051e0120027bcd110c184ab585f0f0
Lennard End <endle@bbcag.ch>
2023-01-22 10:28:53 +0100

    added nodemon run with 'npm run dev'

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs');
+//disable caching
+app.set('etag', false);
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store')
+  next()
+})
 
+//serving static files
 app.use('/public', express.static(`${__dirname}/public`));
 
+//routes for accessing data api
+app.all('/api/:function', (req, res) => {
+  var api = require('./api/api.js');
+  var method = req.method.toLowerCase();
+  res.send(api.resolve(`${method}_${req.params.function}`));
+});
+
+//route for home page
+app.set('view engine', 'ejs');
 app.get('/', (req, res) =>{
   res.render(`${__dirname}/public/stats`, {
     total: 3000,
