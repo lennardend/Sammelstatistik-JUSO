249a49fc5e7ddf45a81cc3bb2e63537a00b112c3
Lennard End <endle@bbcag.ch>
2023-01-22 10:59:19 +0100

    changed db-location

--- database/db.js
+++ database/db.js
@@ -6,20 +6,27 @@ const database = client.db('sammelstatistik');
 const unterschriften = database.collection('unterschriften');
 const ziele = database.collection('ziele');
 
-async function findInZiele(name, callback) {
+async function findInZiele(name) {
     try {
         const query = { "name": name };
         const options = {
             projection: { _id: 0, name: 1, amount: 1 },
           };
         
-        const cursor = await ziele.find(query, options);
-    
-        await cursor.forEach(callback);
+        return await ziele.findOne(query, options);    
     }
     finally {
         await client.close();
     }
 }
 
-module.exports = { findInZiele };
+async function getSignatureAmount() {
+    try {
+        const query = {}
+    }
+    finally {
+        await client.close()
+    }
+}
+
+module.exports = { findInZiele }; 
