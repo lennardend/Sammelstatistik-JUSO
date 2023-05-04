009e04454d884a05fa39f8a3063a4032a432ac16
Lennard End <endle@bbcag.ch>
2023-05-04 15:40:43 +0200

    formatting

--- api/getTop.js
+++ api/getTop.js
@@ -3,29 +3,29 @@ const db = require('../database/db.js');
 async function getData() {
     signatures = await db.aggregateSignatures([
         {
-            "$group": {
-                "_id": "$name", 
-                "amount": {
-                    "$sum": "$amount"
+            '$group': {
+                '_id': '$name',
+                'amount': {
+                    '$sum': '$amount'
                 }
             }
         }, {
-            "$addFields": {
-                "name": "$_id"
+            '$addFields': {
+                'name': '$_id'
             }
         }, {
-            "$unset": "_id"
+            '$unset': '_id'
         }, {
-            "$sort": {
-                "amount": -1, 
-                "name": 1
+            '$sort': {
+                'amount': -1,
+                'name': 1
             }
         }, {
-            "$limit": 5
+            '$limit': 5
         }
     ]);
 
-    return signatures.filter(object => { return object.name !== "Flyers" });
+    return signatures.filter(object => { return object.name !== 'Flyers' });
 }
 
 module.exports = { getData };
